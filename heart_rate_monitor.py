c691b54f5954774aafc615dc08acdc19ff79d318
Ahnaf Tahmid <ahnaftahmid@Ahnafs-MacBook-Air.local>
2024-10-31 00:38:14 +0200

    fix

--- heart_rate_monitor.py
+++ heart_rate_monitor.py
@@ -2,15 +2,15 @@ import time
 import board
 import busio
 from adafruit_ads1x15.analog_in import AnalogIn
-from adafruit_ads1x15.ads1115 import ADS1115
+from adafruit_ads1x15.ads1115 import ADS
 
 # Initialize I2C bus and ADS1115
 i2c = busio.I2C(board.SCL, board.SDA)
-ads = ADS1115(i2c)
+ads = ADS(i2c)
 
 # Set up channels A0 and A1 for testing
-chan_A0 = AnalogIn(ads, ADS1115.P0)
-chan_A1 = AnalogIn(ads, ADS1115.P1)
+chan_A0 = AnalogIn(ads, ADS.P0)
+chan_A1 = AnalogIn(ads, ADS.P1)
 
 try:
     print("Reading raw values from ADS1115 on A0 and A1...")
