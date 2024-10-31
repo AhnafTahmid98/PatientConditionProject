711731589e41821c0e8ee265f075be1e48327ba3
Ahnaf Tahmid <ahnaftahmid@Ahnafs-MacBook-Air.local>
2024-10-31 02:51:33 +0200

    fix

--- heart_rate_monitor.py
+++ heart_rate_monitor.py
@@ -1,13 +1,13 @@
 import time
-from adafruit_ads1x15.ads1x15 import ADS1x15
+from adafruit_ads1x15.ads1115 import ADS1115
 from adafruit_ads1x15.analog_in import AnalogIn
 import board
 import busio
 
 # Setup I2C communication and initialize the ADS1115 ADC
 i2c = busio.I2C(board.SCL, board.SDA)
-ads = ADS1x15(i2c)
-chan = AnalogIn(ads, 0)  # Using channel A0 directly
+ads = ADS1115(i2c)
+chan = AnalogIn(ads, ADS1115.P0)  # Using channel A0
 
 # Variables for pulse detection
 high_threshold = 2.5  # Example voltage threshold for pulse detection
